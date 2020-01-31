c2cab48822a44b843b3b756614058f3b148eaa66
Luccas Calderaro <luccashmc@hotmail.com>
2020-01-31 16:49:59 -0300

    Recurso e Servico de Grupo Procedimento Criado

---
+++ src/main/java/com/luccascalderaro/lc1/resource/GrupoProcedimentoResource.java
@@ -0,0 +1,72 @@
+package com.luccascalderaro.lc1.resource;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.luccascalderaro.lc1.domain.GrupoProcedimento;
+import com.luccascalderaro.lc1.service.GrupoProcedimentoService;
+
+@RestController
+@RequestMapping(value = "/grupoProcedimentos")
+public class GrupoProcedimentoResource {
+
+	@Autowired
+	private GrupoProcedimentoService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<GrupoProcedimento> find(@PathVariable Integer id) {
+
+		GrupoProcedimento obj = service.find(id);
+
+		return ResponseEntity.ok().body(obj);
+
+	}
+
+	@PostMapping
+	private ResponseEntity<Void> insert(@Valid @RequestBody GrupoProcedimento obj) {
+		obj = service.insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody GrupoProcedimento obj, @PathVariable Integer id){
+		
+		obj.setId(id);
+		
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+		
+		
+		
+	
+	
+
+}
---
+++ src/main/java/com/luccascalderaro/lc1/service/GrupoProcedimentoService.java
@@ -0,0 +1,61 @@
+package com.luccascalderaro.lc1.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.luccascalderaro.lc1.domain.GrupoProcedimento;
+import com.luccascalderaro.lc1.repositories.GrupoProcedimentoRepository;
+import com.luccascalderaro.lc1.service.exceptions.DataIntegrityException;
+import com.luccascalderaro.lc1.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class GrupoProcedimentoService {
+
+	@Autowired
+	private GrupoProcedimentoRepository repo;
+
+	public GrupoProcedimento find(Integer id) {
+		Optional<GrupoProcedimento> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Grupo Procedimento nao encontrado"));
+
+	}
+
+	public void updateData(GrupoProcedimento newObj, GrupoProcedimento obj) {
+		newObj.setNomeGrupoProcedimento(obj.getNomeGrupoProcedimento());
+	}
+
+	public GrupoProcedimento update(GrupoProcedimento obj) {
+		GrupoProcedimento newObj = find(obj.getId());
+
+		updateData(newObj, obj);
+
+		return repo.save(newObj);
+
+	}
+
+	public GrupoProcedimento insert(GrupoProcedimento obj) {
+		obj.setId(null);
+		return repo.save(obj);
+
+	}
+	
+	public void delete(Integer id) {
+		try {
+		find(id);
+		repo.deleteById(id);
+		}
+		
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao foi possivel deletar o Grupo de Procedimento");
+			// TODO: handle exception
+		}
+		
+	}
+	
+	
+
+}
