240f7215dff41eca9f6fca009cb36c2f1eebdff3
Luccas Calderaro <luccashmc@hotmail.com>
2020-02-10 20:08:45 -0300

    Refresh Token

---
+++ src/main/java/com/luccascalderaro/lc1/resource/AuthResource.java
@@ -0,0 +1,35 @@
+package com.luccascalderaro.lc1.resource;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luccascalderaro.lc1.security.JWTUtil;
+import com.luccascalderaro.lc1.security.UserSS;
+import com.luccascalderaro.lc1.service.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		 response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+
+	
+	
+
+}
